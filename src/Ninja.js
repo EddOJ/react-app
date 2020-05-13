0d5b0a934df54866922f83be586e1ef163f0f1ea
EddOJ <wolfedd4@gmail.com>
2020-05-13 11:19:18 -0400

    Before major changes

---
+++ src/Ninja.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Ninja extends Component {
+render() {
+	
+	  return (
+	    <div className="App">
+	    	<h1>My First React App</h1>
+	    	<p>Welcome :) </p>
+
+	    </div>
+	  );
+	} 
+}
+
+export default App;
